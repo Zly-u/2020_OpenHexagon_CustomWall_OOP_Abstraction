1d475a592bc6d0982fee0816f2a93f2d888f0f71
Zly <zlyudchik@gmail.com>
2020-08-05 16:09:22 +0300

    example folder init.

---
+++ examples/basicWallManipulation.lua
@@ -0,0 +1,19 @@
+function onInit()
+end
+
+function onLoad()
+end
+
+function addPattern(mKey)
+end
+
+function onUnload()
+    CustomWall.onUnload()
+end
+
+function onUpdate(dt)
+    timer = timer + dt
+    ------------------
+    
+
+end
